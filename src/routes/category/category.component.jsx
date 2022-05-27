a786f74ddeec31c12a2d68f3269d209d46fea6c2
Dorian Thompson <thompsondorian2@gmail.com>
2022-05-27 19:17:17 -0400

    persist cart information after application refresh and refactor asynchronous behavior into redux action

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -7,30 +7,33 @@ import {
     CategoryContainer
 } from './category.styles.jsx';
 
-import { selectCategoriesMap } from '../../store/categories/category.selector.js';
+import { selectCategoriesMap, selectCategoriesIsLoading } from '../../store/categories/category.selector.js';
 
 import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component.jsx';
 
 const Category = () => {
     const { category } = useParams();
-    console.log('render/re-rendering category component');
     const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectCategoriesIsLoading);
     const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
-        console.log('effect fired calling setProducts');
         setProducts(categoriesMap[category]);
     },[category, categoriesMap]);
 
     return (
         <Fragment>
             <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-            <CategoryContainer>
             {
-                products 
-                    && products.map(product => <ProductCard key={product.id} product={product}/>)
+                isLoading ? <Spinner /> : 
+                <CategoryContainer>
+                {
+                    products 
+                        && products.map(product => <ProductCard key={product.id} product={product}/>)
+                }
+                </CategoryContainer>
             }
-            </CategoryContainer>
         </Fragment>
     )
 }
